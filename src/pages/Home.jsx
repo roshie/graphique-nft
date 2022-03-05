929b7d0b8252ac624438ad059d6faa16d38c4772
RAKSHA V G <vgraksha26@gmail.com>
2022-03-05 14:47:44 +0530

    remove layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,15 @@
-import Layout from "../components/Layout";
 import "../assets/css/Home.css";
 
 export default function Home() {
   return (
-    <Layout>
-      <>
-        <div className="d-flex justify-content-center my-4">
-          <div className="card card-banner p-4">
-            <div className="card-body card-text p-lg-5 fs-1 fw-bold">
-              Discover, collect, and sell extraordinary NFTs
-            </div>
+    <>
+      <div className="d-flex justify-content-center my-4">
+        <div className="card card-banner p-4">
+          <div className="card-body card-text p-lg-5 fs-1 fw-bold">
+            Discover, collect, and sell extraordinary NFTs
           </div>
         </div>
-      </>
-    </Layout>
+      </div>
+    </>
   );
 }
