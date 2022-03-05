2b074204349367d7bc8d8a37d2848ec36f932f8c
dhivya910 <dhivyadharshni910@gmail.com>
2022-03-05 14:43:56 +0530

    create items

--- src/components/ItemsCard.jsx
+++ src/components/ItemsCard.jsx
@@ -1,22 +1,22 @@
-
 export function Items(props) {
   return (
-    <div className="card on-hover-scale m-2 card-product">
-      <div className="card-body text-center">
-        <img
-          src={"lucas-benjamin-wQLAGv4_OYs-unsplash.jpg"}
-          style={{
-            width: "862.98px",
-            height: "575px",
-            top: "176px",
-            left: "33.51px",
-            position: "absolute"
-          }}
-          className="card-img-top mb-2 radius-20"
-          alt="Thumbnail"
-        />
+    <>
+      <div className="card on-hover-scale m-2 card-product">
+        <div className="card-body text-center">
+          <img
+            src={"https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
+            style={{
+              width: "862.98px",
+              height: "575px",
+              top: "176px",
+              left: "33.51px",
+              position: "absolute",
+            }}
+            className="card-img-top mb-2 radius-20"
+            alt="Thumbnail"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -12,32 +12,34 @@
 
 export function ProfileCard(props) {
   return (
-    <div className="clearfix">
-      <div className="row">
-        <div className="col-md-4 animated fadeIn">
-          <div className="card">
-            <div className="card-body">
-              <div className="avatar">
-                <img
-                  src={"bulbul-ahmed-cIEb4UJ4ruk-unsplash.jpg"}
-                  className="card-img-top"
-                  alt=""
-                  style={{
-                    width: "88px",
-                    height: "88px",
-                    top: "-1px",
-                    left: "-5px",
-                    position: "absolute"
-                  }}
-                />
+    <>
+      <div className="clearfix">
+        <div className="row">
+          <div className="col-md-4 animated fadeIn">
+            <div className="card">
+              <div className="card-body">
+                <div className="avatar">
+                  <img
+                    src={"https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"}
+                    className="card-img-top"
+                    alt=""
+                    style={{
+                      width: "88px",
+                      height: "88px",
+                      top: "-1px",
+                      left: "-5px",
+                      position: "absolute",
+                    }}
+                  />
+                </div>
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">{props.eth}</p>
               </div>
-              <h5 className="card-title">{props.name}</h5>
-              <p className="card-text">{props.eth}</p>
             </div>
           </div>
+          )
         </div>
-        )
       </div>
-    </div>
+    </>
   );
 }
