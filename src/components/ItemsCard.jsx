5d0128280f3bdce2d358806aaa656d0fc0968327
dhivya910 <dhivyadharshni910@gmail.com>
2022-03-05 10:13:23 +0530

    items img card

---
+++ src/components/ItemsCard.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export function Items(props) {
+  return (
+    <div className="card on-hover-scale m-2 card-product">
+      <div className="card-body text-center">
+        <img
+          src={"lucas-benjamin-wQLAGv4_OYs-unsplash.jpg"}
+          style={{
+            width: "862.98px",
+            height: "575px",
+            top: "176px",
+            left: "33.51px",
+            position: "absolute"
+          }}
+          className="card-img-top mb-2 radius-20"
+          alt="Thumbnail"
+        />
+      </div>
+    </div>
+  );
+}
+
