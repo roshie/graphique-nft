0bf4883e8fbe552f93c3a1781adb12cb23be2803
Roshita <roshitha.23it@licet.ac.in>
2022-03-05 10:22:07 +0530

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { GlobalStyles } from "./global";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
