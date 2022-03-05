b72f837d0aab669eb13407725443ba0f0c60c54a
Roshita <roshitha.23it@licet.ac.in>
2022-03-05 14:33:43 +0530

    remove layouts

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -1,10 +1,9 @@
-import Layout from "../components/Layout";
 export default function Item(props) {
   return (
-    <Layout>
+    <>
       <button onClick={props.toggleTheme}>Toggle theme</button>
       <h1>It's a {props.theme.themeName} theme!</h1>
       <footer></footer>
-    </Layout>
+    </>
   );
 }
