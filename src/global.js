90cf66ba9b3e676e7413323526176bf2a3156c53
Roshita <roshitha.23it@licet.ac.in>
2022-03-05 11:20:35 +0530

    add css files

--- src/global.js
+++ src/global.js
@@ -1,18 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyles = createGlobalStyle(
-  `
+export const GlobalStyles = createGlobalStyle`  
   body {
-    align-items: center;
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 100vh;
-    margin: 0;
-    padding: 0;
-    font-family: 'poppins';
-    transition: all 0.25s linear;
-  }`
-);
+  }`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,18 @@
+import { lightTheme, darkTheme } from "../theme";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const toggleTheme = () => {
+    //   if (theme.themeName === "light") {
+    //     setTheme(darkTheme);
+    //   } else {
+    //     setTheme(lightTheme);
+    //   }
+  };
+  return (
+    <div>
+      <button onClick={toggleTheme}>Toggle theme</button>
+      <h1>It's a {darkTheme.themeName} theme!</h1>
+      <footer></footer>
+    </div>
+  );
 }
