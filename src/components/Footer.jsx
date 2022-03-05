92c6669eeaca46f3964474962bca166cfd0c4699
veroni-d20 <veronishwetha.23it@licet.ac.in>
2022-03-05 11:42:52 +0530

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,48 +1,50 @@
 import React from "react";
-import { logo } from "../../assets/images/logo.svg";
+import Img from "../assets/images/logo-full.svg";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="row">
-        <div className="col-12">
-          <img src={logo} alt="logo" />
-          <p style={{ fontSize: "16px", fontWeight: "600" }}>
-            Get the latest updates
-          </p>
-          <div class="input-group mb-3">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Recipient's username"
-              aria-label="Recipient's username"
-              aria-describedby="basic-addon2"
-            />
-            <div class="input-group-append">
-              <button class="btn btn-outline-secondary" type="button">
-                Email Me!
-              </button>
+    <div className="container fixed-bottom">
+      <footer>
+        <div className="row">
+          <div className="col-12 col-md px-4">
+            <img src={Img} alt="logo" />
+            <p style={{ fontSize: "16px", fontWeight: "600" }}>
+              Get the latest updates
+            </p>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Recipient's username"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button">
+                  Email Me!
+                </button>
+              </div>
             </div>
           </div>
+          <div className="col-6 col-md px-4">
+            <h3>Features</h3>
+            <ul>
+              <li>Explore</li>
+              <li>How it works</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+          <div className="col-6 col-md px-4">
+            <h3>Support</h3>
+            <ul>
+              <li>Help center</li>
+              <li>Terms of service</li>
+              <li>Legal</li>
+              <li>Privacy policy</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-6 col-md-12">
-          <h3>Features</h3>
-          <ul>
-            <li>Explore</li>
-            <li>How it works</li>
-            <li>Contact us</li>
-          </ul>
-        </div>
-        <div className="col-6 col-md-12">
-          <h3>Support</h3>
-          <ul>
-            <li>Help center</li>
-            <li>Terms of service</li>
-            <li>Legal</li>
-            <li>Privacy policy</li>
-          </ul>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
     <>
       <NavBar />
       <div className="container-fluid">{children}</div>
+      <Footer />
     </>
   );
 }
