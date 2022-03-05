9b2044e7a5fba0ff760328d4a4310274ded0e6ef
Roshita <roshitha.23it@licet.ac.in>
2022-03-05 09:40:57 +0530

    create-react-app

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: "#FFFFFF",
+  text: "#363537",
+  primary: "#DA18A3",
+  gradient: "linear-gradient(#EB1484, #C81CC5)",
+};
+
+export const darkTheme = {
+  body: "#24252D",
+  text: "#FAFAFA",
+  primary: "#DA18A3",
+  gradient: "linear-gradient(#EB1484, #C81CC5)",
+};
