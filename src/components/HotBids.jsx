bb18471c607e26be75a9e20e404e9d1e81880f02
RAKSHA V G <vgraksha26@gmail.com>
2022-03-06 10:01:42 +0530

    fix cards

--- src/components/HotBids.jsx
+++ src/components/HotBids.jsx
@@ -11,7 +11,10 @@ export default function HotBids(props) {
         {data &&
           data.map((list, index) => {
             return (
-              <div key={`${list}-${index}`} className="col-6 col-md-3 mb-4">
+              <div
+                key={`${list}-${index}`}
+                className="col-6 col-md-3 p-1 p-md-2"
+              >
                 <Link
                   to={`/item/${index + 1}`}
                   className="text-decoration-none"
@@ -27,7 +30,7 @@ export default function HotBids(props) {
                     />
                     <div className="card-body">
                       <h6
-                        className="card-text fw-bold"
+                        className="card-text p2-bold"
                         style={{
                           color: props.theme.text,
                           textOverflow: "ellipsis",
@@ -39,24 +42,24 @@ export default function HotBids(props) {
                       </h6>
                       <div className="d-flex align-items-center justify-content-between">
                         <p
-                          className="card-text m-0"
-                          style={{ color: props.theme.text, fontSize: "14px" }}
+                          className="card-text m-0 p3-regular"
+                          style={{ color: props.theme.text }}
                         >
-                          <span className="fw-bold"> {list.price}</span>
+                          <span className="p3-bold"> {list.price}</span>
                           &nbsp;ETH
                         </p>
                         <div>
                           <img
                             src={list.liked === true ? heartFill : heart}
                             className="img-fluid"
-                            width="18"
+                            width="16"
                             height="auto"
                             alt={list.name}
                           />
                           <span
+                            className="p3-regular"
                             style={{
                               color: props.theme.text,
-                              fontSize: "14px",
                             }}
                           >
                             &nbsp;{list.likes}
